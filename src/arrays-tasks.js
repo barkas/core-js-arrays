4d56582ab21218770ee6a95644f3cb64653a6602
barkas <vineda.contact@gmail.com>
2024-01-05 17:07:53 +0200

    feat: add sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -41,8 +41,19 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let bigger = arr1;
+  let smaller = arr2;
+  if (arr1.length < arr2.length) {
+    bigger = arr2;
+    smaller = arr1;
+  }
+  return bigger.map((item, ind) => {
+    if (ind < smaller.length) {
+      return item + smaller[ind];
+    }
+    return item;
+  });
 }
 
 /**
