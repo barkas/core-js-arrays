cca2a2c9ea45ec711f95612469d559ee34e8fab1
barkas <vineda.contact@gmail.com>
2024-01-19 15:40:11 +0200

    feat: add generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -369,8 +369,12 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let num = -1;
+  return Array.from({ length: len }, () => {
+    num += 2;
+    return num;
+  });
 }
 
 /**
